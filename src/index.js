4fe765e9007ab2cc63ecd5e5e3753daf58aaf028
Marin M-U <marin.umegane.m@gmail.com>
2024-01-02 11:11:46 -0500

    change router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { HashRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 import "./index.css";
 import App from "./components/App/App";
@@ -9,9 +9,9 @@ import App from "./components/App/App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
